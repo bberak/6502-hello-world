18f627f0df9b825e0f61dc230167ad48245a7e7b
bberak <bberak@gmail.com>
2020-12-09 00:11:30 +1100

    Refactored code with a reusable to_string and print_string function (sub routine). Also refactored some of the other sub routines.

--- src/6502/binary-to-decimal.asm
+++ src/6502/binary-to-decimal.asm
@@ -1,5 +1,11 @@
 !to "build/binary-to-decimal.bin"
 
+;;;;;;;;;;;;;;;;
+;;;; Offset ;;;;
+;;;;;;;;;;;;;;;;
+
+*=$8000
+
 ;;;;;;;;;;;;;;;;;;;
 ;;;; Constants ;;;;
 ;;;;;;;;;;;;;;;;;;;
@@ -13,22 +19,25 @@ E  = %10000000
 RW = %01000000
 RS = %00100000
 
+year_1:
+!word 1955
+
+year_2
+!word 1956
+
+year_3
+!word 1982
+
 ;;;;;;;;;;;;;;;;;;;
 ;;;; Variables ;;;;
 ;;;;;;;;;;;;;;;;;;;
 
-value = $0200
-mod_10 = value + 2
-message = mod_10 + 2
+number = $0200
+mod_10 = number + 2
+string = mod_10 + 2
 
-mem_cmp = message + 6
+mem_cmp = string + 6
 mem_start = mem_cmp + 1
-
-;;;;;;;;;;;;;;;;
-;;;; Offset ;;;;
-;;;;;;;;;;;;;;;;
-
-*=$8000
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Instructions (main) ;;;;
@@ -41,64 +50,64 @@ main:
 
  jsr lcd_init
 
- ; Initialize value to be the number to convert
- lda binary_number
- sta value
- lda binary_number + 1
- sta value + 1
+main_loop:
+ ; Load year_1 into number
+ lda year_1
+ sta number
+ lda year_1 + 1
+ sta number + 1
 
- ; Initialize message
- lda #0
- sta message
+ ; Convert number to string and print
+ jsr to_string
+ jsr print_string
 
-divide:
- ; Initialize the remainder to zero
- lda #0
- sta mod_10
- sta mod_10 + 1
+ jsr delay
+ jsr lcd_clear
+ jsr lcd_return
+ 
+ ; Load year_2 into number
+ lda year_2
+ sta number
+ lda year_2 + 1
+ sta number + 1
 
- ldx #16
- clc 
+ ; Convert number to string and print
+ jsr to_string
+ jsr print_string
 
-division_loop:
- ; Rotate dividend and remainder
- rol value
- rol value + 1
- rol mod_10
- rol mod_10 + 1
+ jsr delay
+ jsr lcd_clear
+ jsr lcd_return
 
- ; a,y = dividend - divisor
- sec
- lda mod_10
- sbc #10
- tay ; Save low byte to Y register
- lda mod_10 + 1
- sbc #0
- bcc ignore_result
- sty mod_10
- sta mod_10 + 1
+ ; Load year_3 into number
+ lda year_3
+ sta number
+ lda year_3 + 1
+ sta number + 1
 
-ignore_result:
- dex 
- bne division_loop
+ ; Convert number to string and print
+ jsr to_string
+ jsr print_string
 
- ; Shift carry bit into value
- rol value
- rol value + 1
+ jsr delay
+ jsr lcd_clear
+ jsr lcd_return
 
-print_remainder:
- clc
- lda mod_10
- adc #"0"
- jsr push_char
+ ; Load literal 07c3 (1987) into number
+ lda #$c3
+ sta number
+ lda #$07
+ sta number + 1
 
- ; If value is not zero, continue dividing (via shift and subtraction)
- lda value
- ora value + 1
- bne divide
+ ; Convert number to string and print
+ jsr to_string
+ jsr print_string
 
- jsr print_message
- jmp idle
+ jsr delay
+ jsr lcd_clear
+ jsr lcd_return
+
+ jmp main_loop
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Memory Utilities ;;;;
@@ -108,32 +117,16 @@ fill_memory_from_x_to_y:
  sta mem_start,x
  stx mem_cmp
  cpy mem_cmp
- beq fill_complete
+ beq fill_memory_from_x_to_y_complete
  inx
  jmp fill_memory_from_x_to_y
 
-fill_complete:
+fill_memory_from_x_to_y_complete:
  rts
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Print Utilities ;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;
-
-print_memory_from_x_to_y:
- pha
-
-print_start:
- lda mem_start,x
- jsr print
- stx mem_cmp
- cpy mem_cmp
- beq print_complete
- inx
- jmp print_start
-
-print_complete:
- pla
- rts
 
 print:
  jsr lcd_wait
@@ -143,6 +136,22 @@ print:
  lda #0
  sta PORTA
  jsr delay
+ rts
+
+print_memory_from_x_to_y:
+ pha
+
+print_memory_from_x_to_y_start:
+ lda mem_start,x
+ jsr print
+ stx mem_cmp
+ cpy mem_cmp
+ beq print_memory_from_x_to_y_complete
+ inx
+ jmp print_memory_from_x_to_y_start
+
+print_memory_from_x_to_y_complete:
+ pla
  rts
 
 print_ascii_table_forever:
@@ -157,19 +166,19 @@ print_random_chars_forever:
  inx
  jmp print_random_chars_forever
 
-print_message:
+print_string:
  pha
  phx
  ldx #0
 
-print_message_loop:
- lda message,x
- beq print_message_break
+print_string_loop:
+ lda string,x
+ beq print_string_break
  jsr print
  inx
- jmp print_message_loop
+ jmp print_string_loop
 
-print_message_break:
+print_string_break:
  plx
  pla
  rts
@@ -179,22 +188,75 @@ print_message_break:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; Add the character in the A register to the beginning
-; of the null-terminated string `message`
+; of the null-terminated variable `string`
 push_char:
  ldy #0 ; Set character index to zero
  
 push_char_loop:
  pha ; Push new character onto the stack
- lda message,y ; Get character at index and push it into X register
+ lda string,y ; Get character at index and push it into X register
  tax 
  pla ; Pop new character off the stack
- sta message,y ; Store new character at the current index
+ sta string,y ; Store new character at the current index
  beq push_char_break
  iny ; Increment index
  txa ; Move previous character into A register
  jmp push_char_loop
 
 push_char_break:
+ rts
+
+to_string:
+ ; Initialize string
+ lda #0
+ sta string
+
+to_string_divide:
+ ; Initialize the remainder to zero
+ lda #0
+ sta mod_10
+ sta mod_10 + 1
+
+ ; Initialize X register to 16 as a count (for processing 2-byte numbers)
+ ldx #16
+ clc 
+
+to_string_division_loop:
+ ; Rotate dividend and remainder
+ rol number
+ rol number + 1
+ rol mod_10
+ rol mod_10 + 1
+
+ ; a,y = dividend - divisor
+ sec
+ lda mod_10
+ sbc #10
+ tay ; Save low byte to Y register
+ lda mod_10 + 1
+ sbc #0
+ bcc to_string_ignore_result
+ sty mod_10
+ sta mod_10 + 1
+
+to_string_ignore_result:
+ dex 
+ bne to_string_division_loop
+
+ ; Shift carry bit into number
+ rol number
+ rol number + 1
+
+to_string_save_remainder:
+ clc
+ lda mod_10
+ adc #"0"
+ jsr push_char
+
+ ; If number is not zero, continue dividing (via shift and subtraction)
+ lda number
+ ora number + 1
+ bne to_string_divide
  rts
 
 ;;;;;;;;;;;;;;;;;;;;;;
@@ -214,14 +276,6 @@ lcd_init:
  lda #%00111000 
  jsr lcd_instruction
 
- ; Clear display
- lda #%00000001 
- jsr lcd_instruction
-
- ; Return home
- lda #%00000010 
- jsr lcd_instruction
-
  ; Display on; cursor on; blink on
  lda #%00001111 
  jsr lcd_instruction
@@ -229,6 +283,21 @@ lcd_init:
  ; Increment cursor; no display shift
  lda #%00000110 
  jsr lcd_instruction
+
+ jsr lcd_clear
+ jsr lcd_return
+
+ rts
+
+lcd_clear:
+ lda #%00000001 
+ jsr lcd_instruction
+ rts
+
+lcd_return:
+ lda #%00000010 
+ jsr lcd_instruction
+ rts
 
 lcd_instruction:
  jsr lcd_wait
@@ -244,14 +313,14 @@ lcd_wait:
  lda #%00000000 ; Set all pins on port B to input
  sta DDRB
 
-lcd_busy:
+lcd_wait_busy:
  lda #RW
  sta PORTA
  lda #(RW | E) ; Toggle RW and E bits to read data
  sta PORTA
  lda PORTB
  and #%10000000
- bne lcd_busy
+ bne lcd_wait_busy
  lda #%11111111 ; Set all pins on port B to output
  sta DDRB
  pla
@@ -283,13 +352,6 @@ delay_break:
 
 idle:
  jmp idle
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;; Post-Instruction ROM ;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-binary_number:
-!word 1729
 
 ;;;;;;;;;;;;;;;;
 ;;;; Offset ;;;;
